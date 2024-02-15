96e8d80e3fb631f3ddb52597c83ca02f0ab410f9
engi-devops <singhsantosh.edu.in@gmail.com>
2024-02-15 09:31:03 +0530

    bug fixed

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,13 @@
 from database.db import db
+from datetime import datetime
 
 class tbl_user(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    phone_number = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.BigInteger, nullable=False)  # Changed to BigInteger
+    username = db.Column(db.String(255))
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<tbl_user %r>' % self.name
